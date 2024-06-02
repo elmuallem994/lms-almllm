cca6a4f819013ec0bbcbb0e8b75a5c1c8fefcd6c
almoallemlamas <elmuallem994@gmail.com>
2024-06-02 19:15:09 +0300

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,6 @@ const nextConfig = {
   images: {
     domains: ["utfs.io"],
   },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/sign-in",
-        permanent: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
